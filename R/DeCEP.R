74200e20af04298c819eed1a91d14a562bb2fbc6
Lin <44568803+Lin-bioinfo@users.noreply.github.com>
2025-03-17 00:07:13 +0800

    Update and rename CoGPS.R to DeCEP.R

---
+++ R/DeCEP.R
@@ -0,0 +1,8 @@
+#' A novel method for deciphering context-specific gene programs using single-cell and spatial transcriptomics data
+#' 
+#' DeCEP provides a computational framework for characterizing context-specific gene programs using scRNA-seq and ST data.
+#' 
+#' @docType package
+#' @name DeCEP-package
+#' @aliases DeCEP
+NULL
